08ff7e48e1ab4449e8052d7b21edf2ae629b8686
kaushiki_khabri <kaushiki.khabri@gmail.com>
2024-12-19 14:23:08 +0530

    movingaverages

--- MovingAverages.py
+++ MovingAverages.py
@@ -5,15 +5,8 @@ def calculate_MA2050100(data):
     data['MA20'] = data['Close'].rolling(window=20).mean()
     data['MA50'] = data['Close'].rolling(window=50).mean()
     data['MA100'] = data['Close'].rolling(window=100).mean()
-    
-    # Reset column names (if necessary)
-    data.columns = [str(col) for col in data.columns]
-    
-    result = data[['Date', 'Close', 'MA20', 'MA50', 'MA100']].dropna().reset_index(drop=True)
-    result_json = result.to_dict(orient="records")
-    return result_json
-
-
+    result = data[['Date', 'Close', 'MA20', 'MA50', 'MA100']].dropna().reset_index()
+    return result
 
 def calculate_MA20(data):
      data['MA20'] = data['Close'].rolling(window=20).mean()
